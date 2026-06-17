0ad5fb2353b520fdcb69a55b64714bc667812c92
Faisal Kherwish <fkherwish@gmail.com>
2026-06-17 07:51:54 -0700

    Installed MongoDB and ready to start sending data to my database.

--- books/books/settings.py
+++ books/books/settings.py
@@ -14,6 +14,9 @@ NEWSPIDER_MODULE = "books.spiders"
 
 ADDONS = {}
 
+MONGO_URI = "mongodb:://localhost:27017"
+MONGO_DATABASE = "books_db"
+
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "books (+http://www.yourdomain.com)"
@@ -58,9 +61,9 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "books.pipelines.BooksPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "books.pipelines.BooksPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
