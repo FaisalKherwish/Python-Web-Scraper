9b9cddb57c6d8f1c1e855cbfb0e359cc1709eb24
Faisal Kherwish <fkherwish@gmail.com>
2026-06-16 21:19:59 -0700

    Added a way for the spider to crawl through multiple pages

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -15,3 +15,9 @@ class BookSpider(scrapy.Spider):
             item["title"] = book.css("h3 > a::attr(title)").get()
             item["price"] = book.css(".price_color::text").get()
             yield item
+
+        next_page_url = response.css("li.next > a::attr(href)").get()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
+    
